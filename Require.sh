1f0fdab43de26c95460a7b276ea9224ee1e39739
Er. RAHUL KUMAR JHA | B.E. | CCEP | CCPP | CTIGA |Cyber Security Specialist | CyberJhaJi | Security Researcher | Awarded 🏆Best Outgoing Student In Engineering <142599405+SilentProfessor@users.noreply.github.com>
2026-01-20 19:04:16 +0530

    Update Require.sh

--- Require.sh
+++ Require.sh
@@ -1,2 +1,4 @@
+sudo apt update && apt upgrade && apt-get Update && apt-get Upgrade
+sudo apt install python3 pip3 git
 sudo pip install --upgrade pip
 sudo pip install scapy requests threading multiprocessing psutil
